03ae8fb25e19749aceb58bce98ce11741a50e70e
Sweeper777 <sumulang@gmail.com>
2016-07-20 08:09:27 +0800

    added a simple test

--- src/com/verbosy/Main.java
+++ src/com/verbosy/Main.java
@@ -1,8 +1,26 @@
 package com.verbosy;
 
+import com.verbosy.compiler.CompilerUtility;
+import com.verbosy.instructions.primitive.Instruction;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String[] arr = "~0 /14 :a: ^14 i >0b: /14* >a: :b: v14 :c: \\14* o v14 >0a: >c:".split(" ");
+        ArrayList<String> instructionNames = new ArrayList<>();
+	    for (String instruction : arr) {
+            for (Pattern p : CompilerUtility.getInstructionMap().keySet()) {
+                if (p.matcher(instruction).matches()) {
+                    Class<? extends Instruction> instructionType = CompilerUtility.getInstructionMap().get(p);
+                    instructionNames.add(instructionType.getSimpleName());
+                    break;
+                }
+            }
+        }
+
+        instructionNames.forEach(System.out::println);
     }
 }
