36d592efcbd0bd9454b308fda173c1a9618aba22
Sweeper777 <sumulang@gmail.com>
2016-07-21 11:31:20 +0800

    added hashcode methods in case I need them

--- src/com/verbosy/instructions/primitive/Label.java
+++ src/com/verbosy/instructions/primitive/Label.java
@@ -23,4 +23,22 @@ public class Label implements Instruction {
     public void execute(VerbosyRuntime runtime) {
         // do nothing
     }
+
+    @Override
+    public int hashCode() {
+        return getName().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+
+        if (!(obj instanceof Label)) {
+            return false;
+        }
+
+        return getName().equals(((Label)obj).getName());
+    }
 }
