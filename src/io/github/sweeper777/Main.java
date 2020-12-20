32a7e331a502deea07f85f02050ab9e0ae8bc1d9
Mulang Su <sumulang@gmail.com>
2020-12-20 15:51:00 +0800

    added some ideas for compiler options

--- src/io/github/sweeper777/Main.java
+++ src/io/github/sweeper777/Main.java
@@ -8,6 +8,14 @@ import java.io.IOException;
 
 public class Main {
 
+    /*
+    Command Line options:
+    --input, -i: input, use stdin if empty
+    --memory-size, -s: number of elements in the memory array
+    --space-as-zero, -z: read space characters as 0s
+    --compile, -c: compile to binary file
+    --run-compiled, -r: run a binary verbosy program
+     */
     public static void main(String[] args) {
         try {
             VerbosyProgram prog = VerbosyProgram.fromSourceFile(args[0]);
