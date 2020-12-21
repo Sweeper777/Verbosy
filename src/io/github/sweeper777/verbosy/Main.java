d78c444626e080408b1f513a6fd4145e9ea5b838
Mulang Su <sumulang@gmail.com>
2020-12-21 10:08:59 +0800

    parse command line options

--- src/io/github/sweeper777/verbosy/Main.java
+++ src/io/github/sweeper777/verbosy/Main.java
@@ -3,6 +3,7 @@ package io.github.sweeper777.verbosy;
 import io.github.sweeper777.verbosy.compiler.CompilerErrorException;
 import io.github.sweeper777.verbosy.compiler.VerbosyProgram;
 import io.github.sweeper777.verbosy.runtime.StandardRuntime;
+import org.apache.commons.cli.*;
 
 import java.io.IOException;
 
@@ -17,14 +18,33 @@ public class Main {
     --run-compiled, -r: run a binary verbosy program
      */
     public static void main(String[] args) {
+        CommandLineParser parser = new DefaultParser();
         try {
-            VerbosyProgram prog = VerbosyProgram.fromSourceFile(args[0]);
-            StandardRuntime runtime = new StandardRuntime(System.in, 10);
-            prog.run(runtime);
+            Options cliOptions = getCommandLineOptions();
+            CommandLine cl = parser.parse(cliOptions, args, true);
+            if (cl.hasOption("help")) {
+                HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp("verbosy", cliOptions, true);
+            } else if (cl.hasOption('c')) {
+
+            } else {
+                runVerbosySourceFile(cl);
+            }
+
+        } catch (ParseException e) {
+            System.err.println("Exception occurred while parsing command line arguments.");
+            System.err.println(e.getMessage());
         } catch (IOException | ClassNotFoundException | CompilerErrorException e) {
             e.printStackTrace();
         }
     }
+
+    private static void runVerbosySourceFile(CommandLine cl) throws CompilerErrorException, IOException, ClassNotFoundException {
+        VerbosyProgram prog = VerbosyProgram.fromSourceFile(cl.getArgs()[0]);
+        StandardRuntime runtime = new StandardRuntime(System.in, 10);
+        prog.run(runtime);
+    }
+
     private static Options getCommandLineOptions() {
         Option input = Option.builder("i")
                 .argName("input")
