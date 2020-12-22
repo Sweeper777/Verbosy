e53b914c24b8f61480672c37536df8f1a639874a
Mulang Su <sumulang@gmail.com>
2020-12-22 09:39:23 +0800

    added test skeleton

---
+++ src/io/github/sweeper777/verbosy/tests/MainTest.java
@@ -0,0 +1,12 @@
+package io.github.sweeper777.verbosy.tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void canRunSourceFile() {
+    }
+}
