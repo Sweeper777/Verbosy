ffb7cb6a3bc519810d3e9f6bec8b7382e00f0abd
Mulang Su <sumulang@gmail.com>
2020-12-23 09:48:18 +0800

    tested running hello world

--- src/io/github/sweeper777/verbosy/tests/MainTest.java
+++ src/io/github/sweeper777/verbosy/tests/MainTest.java
@@ -1,12 +1,22 @@
 package io.github.sweeper777.verbosy.tests;
 
+import io.github.sweeper777.verbosy.compiler.CompilerErrorException;
+import io.github.sweeper777.verbosy.compiler.VerbosyProgram;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
 
 public class MainTest {
 
     @Test
-    public void canRunSourceFile() {
+    public void helloWorld() throws IOException, CompilerErrorException {
+        VerbosyProgram program = VerbosyProgram.fromSourceFile("src/io/github/sweeper777/verbosy/tests/helloworld.vp");
+        TestRuntime runtime = new TestRuntime("", 10);
+        program.run(runtime);
+        assertEquals("HelloWorld", runtime.getOutputString());
+    }
+
     }
 }
