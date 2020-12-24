4c6c470e1dd3e88ed1765ff058e73784352fe3e1
Mulang Su <sumulang@gmail.com>
2020-12-24 08:46:10 +0800

    tested reading 2 numbers from runtime

---
+++ src/io/github/sweeper777/verbosy/tests/StandardRuntimeTest.java
@@ -0,0 +1,20 @@
+package io.github.sweeper777.verbosy.tests;
+
+import io.github.sweeper777.verbosy.runtime.StandardRuntime;
+import io.github.sweeper777.verbosy.runtime.VerbosyValue;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class StandardRuntimeTest {
+
+    @Test
+    public void read2Numbers() {
+        StandardRuntime runtime = new StandardRuntime("12345 67890", 10);
+        VerbosyValue input = runtime.getNextInput();
+        assertEquals(new VerbosyValue(12345), input);
+        input = runtime.getNextInput();
+        assertEquals(new VerbosyValue(67890), input);
+    }
+
+}
