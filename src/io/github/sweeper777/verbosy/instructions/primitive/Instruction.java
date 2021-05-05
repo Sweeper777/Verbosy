1772af0093c05bd3af997021146e61a06caa05b0
Mulang Su <sumulang@gmail.com>
2021-05-05 11:35:16 +0800

    started the rewrite to compile to C

--- src/io/github/sweeper777/verbosy/instructions/primitive/Instruction.java
+++
@@ -1,7 +0,0 @@
-package io.github.sweeper777.verbosy.instructions.primitive;
-
-import io.github.sweeper777.verbosy.runtime.VerbosyRuntime;
-
-public interface Instruction {
-    void execute(VerbosyRuntime runtime);
-}
--- src/io/github/sweeper777/verbosy/instructions/primitive/ParameterInstruction.java
+++
@@ -1,7 +0,0 @@
-package io.github.sweeper777.verbosy.instructions.primitive;
-
-import io.github.sweeper777.verbosy.runtime.VerbosyParameter;
-
-public interface ParameterInstruction extends Instruction {
-    VerbosyParameter getParameter();
-}
