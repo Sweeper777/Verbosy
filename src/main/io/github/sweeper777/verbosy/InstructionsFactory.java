f3ba1d4d9f007f88bfbcbf4cade7f94da95ae0d6
Mulang Su <sumulang@gmail.com>
2021-05-07 08:49:12 +0800

    printed info when exiting every rule

--- src/main/io/github/sweeper777/verbosy/InstructionsFactory.java
+++ src/main/io/github/sweeper777/verbosy/InstructionsFactory.java
@@ -1,11 +1,31 @@
 package io.github.sweeper777.verbosy;
 
-import io.github.sweeper777.verbosy.VerbosyParser.InstructionContext;
+import io.github.sweeper777.verbosy.VerbosyParser.Set_instructionContext;
+import org.antlr.v4.runtime.Parser;
+import org.antlr.v4.runtime.ParserRuleContext;
 
 public class InstructionsFactory extends VerbosyBaseListener {
 
+  private final Parser p;
+
+  public InstructionsFactory(Parser p) {
+    this.p = p;
+  }
+
   @Override
-  public void exitInstruction(InstructionContext ctx) {
-    System.out.println(ctx.getText());
+  public void exitSet_instruction(Set_instructionContext ctx) {
+//    System.out.println("Found Set Instruction");
+//    VerbosyParser.Set_character_argumentContext charArg = ctx.set_character_argument();
+//    VerbosyParser.Set_integer_argumentContext intArg = ctx.set_integer_argument();
+//    if (charArg != null) {
+//      System.out.println("Character argument: " + charArg.getText());
+//    } else if (intArg != null) {
+//      System.out.println("Integer argument: " + intArg.getText());
+//    }
+  }
+
+  @Override
+  public void enterEveryRule(ParserRuleContext ctx) {
+    System.out.println(ctx.toInfoString(p));
   }
 }
