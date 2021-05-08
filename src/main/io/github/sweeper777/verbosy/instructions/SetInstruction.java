c346bd485eb305f5985068e0ea57d154436f7ac2
Mulang Su <sumulang@gmail.com>
2021-05-08 08:33:30 +0800

    Create SetInstruction.java

---
+++ src/main/io/github/sweeper777/verbosy/instructions/SetInstruction.java
@@ -0,0 +1,53 @@
+package io.github.sweeper777.verbosy.instructions;
+
+import io.github.sweeper777.verbosy.VerbosyValue;
+
+public class SetInstruction implements Instruction {
+
+  private final int lineNo;
+  private final int columnNo;
+  private final VerbosyValue parameter;
+
+  public int getLineNo() {
+    return lineNo;
+  }
+
+  public int getColumnNo() {
+    return columnNo;
+  }
+
+  public VerbosyValue getParameter() {
+    return parameter;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    SetInstruction that = (SetInstruction) o;
+
+    return parameter.equals(that.parameter);
+  }
+
+  @Override
+  public int hashCode() {
+    return parameter.hashCode();
+  }
+
+  public SetInstruction(int lineNo, int columnNo,
+      VerbosyValue parameter) {
+    this.lineNo = lineNo;
+    this.columnNo = columnNo;
+    this.parameter = parameter;
+  }
+
+  @Override
+  public String toString() {
+    return "SET " + getParameter();
+  }
+}
