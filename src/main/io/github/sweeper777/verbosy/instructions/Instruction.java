7a2fbcf9bd06b8de8775ed33f64b72851108fa6c
Mulang Su <sumulang@gmail.com>
2021-05-15 09:58:36 +0800

    added getCode requirement in Instruction

--- src/main/io/github/sweeper777/verbosy/instructions/Instruction.java
+++ src/main/io/github/sweeper777/verbosy/instructions/Instruction.java
@@ -1,6 +1,10 @@
 package io.github.sweeper777.verbosy.instructions;
 
+import io.github.sweeper777.verbosy.CodeProvider;
+
 public interface Instruction {
   int getLineNo();
   int getColumnNo();
+
+  String getCode(CodeProvider provider);
 }
