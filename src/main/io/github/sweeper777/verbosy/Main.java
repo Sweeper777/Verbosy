6cf19bbb29fbe1f5308d48d6f9e5c4fa838a84c0
Mulang Su <sumulang@gmail.com>
2021-05-24 08:40:03 +0800

    checked for no warning option

--- src/main/io/github/sweeper777/verbosy/Main.java
+++ src/main/io/github/sweeper777/verbosy/Main.java
@@ -48,7 +48,7 @@ public class Main {
         var codeProvider = new CSharpCodeProvider(
             memorySize, cl.hasOption('z'), cl.hasOption('i')
         );
-        var compiler = new VerbosyCompiler(memorySize, codeProvider, true);
+        var compiler = new VerbosyCompiler(memorySize, codeProvider, !cl.hasOption('n'));
         String outputFile = "a.out";
         if (cl.hasOption('o')) {
             outputFile = cl.getOptionValue('o');
