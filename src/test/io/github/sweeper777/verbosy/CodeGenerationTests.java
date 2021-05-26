cc57e5d7ff93fd5211cfef8c63071a0ac2866329
Mulang Su <sumulang@gmail.com>
2021-05-26 10:23:57 +0800

    added testIO

---
+++ src/test/io/github/sweeper777/verbosy/CodeGenerationTests.java
@@ -0,0 +1,21 @@
+package io.github.sweeper777.verbosy;
+
+import static io.github.sweeper777.verbosy.TestUtils.runCode;
+import static org.junit.Assert.assertEquals;
+
+import io.github.sweeper777.verbosy.compiler.VerbosyCompiler;
+import io.github.sweeper777.verbosy.csharp.CSharpCodeProvider;
+import java.io.IOException;
+import org.junit.Test;
+
+public class CodeGenerationTests {
+
+  @Test
+  public void testIO() throws IOException, InterruptedException {
+    var compiler = new VerbosyCompiler(1024,
+        new CSharpCodeProvider(1024, false, false), false);
+    assertEquals("h", runCode("i o", "hello", compiler));
+    assertEquals("hello", runCode(":a: i o >a", "hello", compiler));
+  }
+
+}
