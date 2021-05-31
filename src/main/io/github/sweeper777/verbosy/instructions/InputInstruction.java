737c3143adf0be4cd3fc0f9c789361c3af14e9b0
Mulang Su <sumulang@gmail.com>
2021-05-31 09:16:49 +0800

    made input and output subclasses of parameterless instruction

--- src/main/io/github/sweeper777/verbosy/instructions/InputInstruction.java
+++ src/main/io/github/sweeper777/verbosy/instructions/InputInstruction.java
@@ -2,35 +2,11 @@ package io.github.sweeper777.verbosy.instructions;
 
 import io.github.sweeper777.verbosy.compiler.CodeProvider;
 
-public class InputInstruction implements Instruction {
+public class InputInstruction extends ParameterlessInstruction {
 
-  private final int lineNo;
-  private final int columnNo;
-
-  public int getLineNo() {
-    return lineNo;
-  }
-
-  public int getColumnNo() {
-    return columnNo;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    return o != null && getClass() == o.getClass();
-  }
-
-  @Override
-  public int hashCode() {
-    return 0;
-  }
 
   public InputInstruction(int lineNo, int columnNo) {
-    this.lineNo = lineNo;
-    this.columnNo = columnNo;
+    super(lineNo, columnNo);
   }
 
   @Override
--- src/main/io/github/sweeper777/verbosy/instructions/OutputInstruction.java
+++ src/main/io/github/sweeper777/verbosy/instructions/OutputInstruction.java
@@ -2,35 +2,10 @@ package io.github.sweeper777.verbosy.instructions;
 
 import io.github.sweeper777.verbosy.compiler.CodeProvider;
 
-public class OutputInstruction implements Instruction {
-
-  private final int lineNo;
-  private final int columnNo;
-
-  public int getLineNo() {
-    return lineNo;
-  }
-
-  public int getColumnNo() {
-    return columnNo;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-    return o != null && getClass() == o.getClass();
-  }
-
-  @Override
-  public int hashCode() {
-    return 0;
-  }
+public class OutputInstruction extends ParameterlessInstruction {
 
   public OutputInstruction(int lineNo, int columnNo) {
-    this.lineNo = lineNo;
-    this.columnNo = columnNo;
+    super(lineNo, columnNo);
   }
 
   @Override
