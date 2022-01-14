24caa7f0eaf17bf2d4e203b918b9fe0be711bb78
Mulang Su <sumulang@gmail.com>
2022-01-14 15:24:57 +0000

    Create BasicBlock.java

---
+++ src/main/io/github/sweeper777/verbosy/compiler/BasicBlock.java
@@ -0,0 +1,28 @@
+package io.github.sweeper777.verbosy.compiler;
+
+import io.github.sweeper777.verbosy.instructions.Instruction;
+
+public class BasicBlock {
+
+  private final int startIndex;
+  private final int endIndexExclusive;
+  private final ControlFlowGraph cfg;
+
+  public BasicBlock(int startIndex, int endIndexExclusive, ControlFlowGraph cfg) {
+    this.startIndex = startIndex;
+    this.endIndexExclusive = endIndexExclusive;
+    this.cfg = cfg;
+  }
+
+  public int getStartIndex() {
+    return startIndex;
+  }
+
+  public int getEndIndexExclusive() {
+    return endIndexExclusive;
+  }
+
+  public ControlFlowGraph getCFG() {
+    return cfg;
+  }
+}
