e15ac5203fb491429a8101f9cca824d7adf70629
Mulang Su <sumulang@gmail.com>
2022-01-19 22:03:38 +0000

    slightly changed the design of CFG

--- src/main/io/github/sweeper777/verbosy/compiler/ControlFlowGraph.java
+++ src/main/io/github/sweeper777/verbosy/compiler/ControlFlowGraph.java
@@ -1,18 +1,27 @@
 package io.github.sweeper777.verbosy.compiler;
 
+import io.github.sweeper777.verbosy.instructions.GotoInstruction;
+import io.github.sweeper777.verbosy.instructions.GotoInstructionBase;
+import io.github.sweeper777.verbosy.instructions.HaltInstruction;
 import io.github.sweeper777.verbosy.instructions.Instruction;
+import io.github.sweeper777.verbosy.instructions.LabelInstruction;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ControlFlowGraph {
   private final List<BasicBlock> basicBlocks = new ArrayList<>();
-  private final List<Instruction> sourceCode;
-  private final HashMap<BasicBlock, List<BasicBlock>> successors = new HashMap<>();
+  private final List<Instruction> sourceCode = new ArrayList<>();
+  private final HashMap<BasicBlock, HashSet<BasicBlock>> successors = new HashMap<>();
 
-  public ControlFlowGraph(List<Instruction> code) {
-    this.sourceCode = code;
+  private ControlFlowGraph() {
   }
 
   public List<Instruction> getSourceCode() {
