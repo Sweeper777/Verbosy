5d8f6f0c1397c83b0ffb7b1743a8b76ee4af5360
Mulang Su <sumulang@gmail.com>
2022-01-21 21:34:59 +0000

    tested generatesCFGForWhileLoop

--- src/test/io/github/sweeper777/verbosy/CFGTests.java
+++ src/test/io/github/sweeper777/verbosy/CFGTests.java
@@ -109,4 +109,38 @@ public class CFGTests {
     assertEquals(8, cfg.getBasicBlock(3).getEndIndexExclusive());
   }
 
+  @Test
+  public void generatesCFGForWhileLoop() {
+    var analyser = new SemanticAnalyer(
+        List.of(
+            label("foo"),
+            add(20, false),
+            add(20, false),
+            goToIf0("bar"),
+            add(20, false),
+            goTo("foo"),
+            label("bar")
+        ),
+        new ArrayList<>(),
+        100,
+        true);
+    analyser.analyseSemantics();
+    var cfg = analyser.getCFG();
+    assertEquals(4, cfg.getBasicBlocks().size());
+
+    assertEquals(Set.of(cfg.getBasicBlock(1), cfg.getBasicBlock(2)), cfg.getSuccessor(0));
+    assertEquals(Set.of(cfg.getBasicBlock(0)), cfg.getSuccessors().get(cfg.getBasicBlock(1)));
+    assertEquals(Set.of(cfg.getBasicBlock(3)), cfg.getSuccessors().get(cfg.getBasicBlock(2)));
+    assertEquals(Set.of(), cfg.getSuccessors().get(cfg.getBasicBlock(3)));
+
+    assertEquals(0, cfg.getBasicBlock(0).getStartIndex());
+    assertEquals(4, cfg.getBasicBlock(0).getEndIndexExclusive());
+    assertEquals(4, cfg.getBasicBlock(1).getStartIndex());
+    assertEquals(6, cfg.getBasicBlock(1).getEndIndexExclusive());
+    assertEquals(6, cfg.getBasicBlock(2).getStartIndex());
+    assertEquals(7, cfg.getBasicBlock(2).getEndIndexExclusive());
+    assertEquals(7, cfg.getBasicBlock(3).getStartIndex());
+    assertEquals(7, cfg.getBasicBlock(3).getEndIndexExclusive());
+  }
+
 }
