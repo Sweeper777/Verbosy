bbff77a99b0871d46638f3a99fe2da7fa575fc9f
Mulang Su <sumulang@gmail.com>
2022-03-03 22:21:09 +0000

    used backend in VerbosyCompiler

--- src/main/io/github/sweeper777/verbosy/VerbosyCompiler.java
+++ src/main/io/github/sweeper777/verbosy/VerbosyCompiler.java
@@ -1,32 +1,29 @@
 package io.github.sweeper777.verbosy;
 
-import io.github.sweeper777.verbosy.codegen.CodeGenerator;
-import io.github.sweeper777.verbosy.codegen.CodeProvider;
+import io.github.sweeper777.verbosy.codegen.CompilerBackend;
 import io.github.sweeper777.verbosy.semantics.SemanticAnalyer;
 import io.github.sweeper777.verbosy.syntax.InstructionsFactory;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class VerbosyCompiler {
   private int memorySize;
-  private final CodeProvider provider;
+  private final CompilerBackend backend;
   private boolean generateWarnings = true;
   private boolean eliminateDeadCode = true;
   private final List<Diagnostic> diagnostics = new ArrayList<>();
 
-  public VerbosyCompiler(int memorySize, CodeProvider provider) {
+  public VerbosyCompiler(int memorySize, CompilerBackend backend) {
     this.memorySize = memorySize;
-    this.provider = provider;
+    this.backend = backend;
   }
 
-  public void compile(CharStream stream, String outputFileName, String outputSourceFile) throws IOException {
+  public void compile(CharStream stream, String outputFileName) throws IOException {
     VerbosyLexer lexer = new VerbosyLexer(stream);
     CommonTokenStream tokenStream = new CommonTokenStream(lexer);
     VerbosyParser parser = new VerbosyParser(tokenStream);
@@ -42,40 +39,12 @@ public class VerbosyCompiler {
     }
     diagnostics.forEach(System.err::println);
     if (diagnostics.stream().noneMatch(x -> x.getType() == Diagnostic.Type.ERROR)) {
-      File sourceFile;
-      if (outputSourceFile == null) {
-        sourceFile = File.createTempFile("verbosyOutput", ".cs");
-      } else {
-        sourceFile = new File(outputSourceFile);
-      }
-      try (var file = new FileOutputStream(sourceFile)) {
-        var codeGen = new CodeGenerator(instructions, provider, file);
-        codeGen.generateCode();
-      }
-      Runtime runtime = Runtime.getRuntime();
-      Process p = runtime.exec("csc " + sourceFile.getAbsolutePath() + " -warn:0 -out:" + outputFileName);
-      try {
-        p.waitFor();
-        if (p.exitValue() != 0) {
-          System.out.println("Something wrong happened while generating IL!");
-          System.err.write(p.getInputStream().readAllBytes());
-        }
-      } catch (InterruptedException ex) {
-        System.err.println("Interrupted when waiting for code to compile!");
-      } finally {
-        if (outputSourceFile == null) {
-          sourceFile.delete();
-        }
-      }
+      backend.generateCode(instructions, outputFileName);
     }
   }
 
   public List<Diagnostic> getDiagnostics() {
     return diagnostics;
-  }
-
-  public void compile(CharStream stream, String outputFileName) throws IOException {
-    compile(stream, outputFileName, null);
   }
 
   public int getMemorySize() {
