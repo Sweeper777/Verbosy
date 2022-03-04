562686109ea7096a1191e19fd16f7a16c730afdc
Mulang Su <sumulang@gmail.com>
2022-03-04 22:26:33 +0000

    tried generating a simple hello world class

--- src/main/io/github/sweeper777/verbosy/codegen/jvm/JvmCodeGenerator.java
+++ src/main/io/github/sweeper777/verbosy/codegen/jvm/JvmCodeGenerator.java
@@ -28,6 +28,37 @@ public class JvmCodeGenerator implements CompilerBackend {
         verbosyValueClass.setName("VerbosyValue");
         utilsClass.setName("Util");
 
+        try {
+            var mainClass = new ClassFile(false, "VerbosyProgram", null);
+            var constPool = mainClass.getConstPool();
+            var mainMethodCode = new Bytecode(constPool);
+
+            for (var instruction: instructions) {
+                generateCodeForInstruction(mainMethodCode, instruction);
+            }
+            int label = mainMethodCode.currentPc();
+            mainMethodCode.addGetstatic(ClassPool.getDefault().get("java.lang.System"), "out", "Ljava/io/PrintStream;");
+            mainMethodCode.addLdc("Hello World!");
+            mainMethodCode.addStore(1, ClassPool.getDefault().get("java.lang.String"));
+            mainMethodCode.addLoad(1, ClassPool.getDefault().get("java.lang.String"));
+            mainMethodCode.addInvokevirtual("java.io.PrintStream", "println", "(Ljava/lang/String;)V");
+            mainMethodCode.addOpcode(Opcode.GOTO);
+            var offset = label - mainMethodCode.currentPc() + 1;
+            mainMethodCode.addIndex(offset);
+            var stackMap = new StackMapTable.Writer(0);
+            stackMap.sameFrame(0);
+
+            mainMethodCode.setMaxLocals(2);
+            var mainMethodInfo = new MethodInfo(constPool, "main", "([Ljava/lang/String;)V");
+            var codeAttr = mainMethodCode.toCodeAttribute();
+            codeAttr.setAttribute(stackMap.toStackMapTable(constPool));
+            mainMethodInfo.setCodeAttribute(codeAttr);
+            mainClass.addMethod(mainMethodInfo);
+            mainClass.setAccessFlags(AccessFlag.PUBLIC);
+            mainMethodInfo.setAccessFlags(AccessFlag.PUBLIC | AccessFlag.STATIC);
+        } catch (CannotCompileException | NotFoundException e) {
+            throw new IOException(e);
+        }
     }
 
 }
