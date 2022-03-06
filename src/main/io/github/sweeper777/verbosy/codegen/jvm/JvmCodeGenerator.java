90c9a01d526e7cf43b9a830d41689c94313fc1e5
Mulang Su <sumulang@gmail.com>
2022-03-06 22:28:30 +0000

    setup JvmCodeGenerator

--- src/main/io/github/sweeper777/verbosy/codegen/jvm/JvmCodeGenerator.java
+++ src/main/io/github/sweeper777/verbosy/codegen/jvm/JvmCodeGenerator.java
@@ -2,11 +2,26 @@ package io.github.sweeper777.verbosy.codegen.jvm;
 
 import io.github.sweeper777.verbosy.codegen.CompilerBackend;
 import io.github.sweeper777.verbosy.syntax.Instruction;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Type;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
+import static org.objectweb.asm.Opcodes.*;
+
 public class JvmCodeGenerator implements CompilerBackend {
+    public JvmCodeProvider getCodeProvider() {
+        return codeProvider;
+    }
+
+    private final JvmCodeProvider codeProvider;
+
+    public JvmCodeGenerator(boolean readSpaceAsZero, boolean readInts) {
+        codeProvider = new DefaultJvmCodeProvider(readSpaceAsZero, readInts);
+    }
+
     @Override
     public void generateCode(List<Instruction> instructions, String outputFileName) throws IOException {
 
