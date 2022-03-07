335491453950a4d2c9d572ecd08d29db412319d9
Mulang Su <sumulang@gmail.com>
2022-03-07 21:33:10 +0000

    generated get current

--- src/main/io/github/sweeper777/verbosy/codegen/jvm/DefaultJvmCodeProvider.java
+++ src/main/io/github/sweeper777/verbosy/codegen/jvm/DefaultJvmCodeProvider.java
@@ -44,6 +44,10 @@ public class DefaultJvmCodeProvider implements JvmCodeProvider {
         this.readInts = readInts;
     }
 
+    private void generateGetCurrent(MethodVisitor mv) {
+        mv.visitFieldInsn(GETSTATIC, UTIL_CLASS, UTIL_CURRENT, Type.getObjectType(VERBOSY_VALUE_CLASS).getDescriptor());
+    }
+
     private void generateNullCheckForCurrent(MethodVisitor mv) {
         mv.visitFieldInsn(GETSTATIC, UTIL_CLASS, UTIL_CURRENT, Type.getObjectType(VERBOSY_VALUE_CLASS).getDescriptor());
         mv.visitJumpInsn(IFNULL, lastLabel);
